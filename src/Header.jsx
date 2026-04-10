eec89c9e201295a30b96d4345d9d048897bf5fa9
Chon <bonuz@gmail.com>
2026-04-10 15:49:46 +0700

    fordeploy

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header(){
+
+    return(
+        <header>
+            <h1>My web</h1>
+            <nav>
+                <ul>my list
+                <li>list1</li>
+                <li>list2</li>
+            </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
