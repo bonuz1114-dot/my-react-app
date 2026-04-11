3078eeb3fa2b14a2de81ca1b13de6fdd520964cc
Chon <bonuz@gmail.com>
2026-04-11 18:35:51 +0700

    3 red button

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -6,22 +6,17 @@ const DashboardButton = () => {
   const [count, setCount] = useState(0)
   return (
     <>
-    <div className='mybutton'>
-      <button 
-      className="dashboard-btn"
-    >
-      <h1>กด</h1>
-     </button>
-     <button 
-      className="dashboard-btn"
-    >
-      <h1>กด</h1>
-      </button>
-      <button 
-      className="dashboard-btn"
-    >
-      <h1>กด</h1> 
+    <div className='flex flex-col items-center gap-4 w-full'>
+      <button className="bg-red-600 hover:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
+        เริ่มใช้งานเลย
       </button> 
+      <button className="bg-red-600 hover:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
+        เริ่มใช้งานเลย
+      </button> 
+      <button className="bg-red-600 hover:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
+        เริ่มใช้งานเลย
+      </button> 
+    
     </div>
     </>
   );
