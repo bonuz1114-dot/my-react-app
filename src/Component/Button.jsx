2c2196814e01a77daa7aed2a00c5f438f6b41e7f
Chon <bonuz@gmail.com>
2026-04-11 18:53:02 +0700

    to deploy for phone

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -7,13 +7,13 @@ const DashboardButton = () => {
   return (
     <>
     <div className='flex flex-col items-center gap-4 w-full'>
-      <button className="bg-red-600 hover:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
+      <button className="bg-red-600 hover:bg-amber-500 active:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
         เริ่มใช้งานเลย
       </button> 
-      <button className="bg-red-600 hover:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
+      <button className="bg-red-600 hover:bg-amber-500 active:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
         เริ่มใช้งานเลย
       </button> 
-      <button className="bg-red-600 hover:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
+      <button className="bg-red-600 hover:bg-amber-500 active:bg-amber-500 text-white font-medium py-6 px-6 rounded-full shadow-lg transition hover:scale-95 active:scale-95">
         เริ่มใช้งานเลย
       </button> 
     
